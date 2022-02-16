5f5f79d3bc816836840f7204cd383548d2d21ac5
Пользователь <braginets551@gmail.com>
2022-02-16 10:25:30 +0200

    Static library for task 8.1

--- lab08/prj/ModulesBraginets/main.cpp
+++ lab08/prj/ModulesBraginets/main.cpp
@@ -5,6 +5,6 @@ const double PI = 3.14159265;
 double s_calculation(double x, double y, double z)
 {
     return 1 + sqrt(pow(x / abs(y - z), 3) +
-            (pow(x, 2) + z) / sqrt(abs((z + x) * y)) -
+          (pow(x, 2) + z) / sqrt(abs((z + x) * y)) -
             cos(x * pow(z, 2)) / (2 * y + PI));
 }
