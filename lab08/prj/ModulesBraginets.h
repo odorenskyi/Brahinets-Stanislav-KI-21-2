b1242c60a1241a13e55c1624302d104713ad3ecf
Пользователь <braginets551@gmail.com>
2022-03-18 16:16:22 +0200

    Added three prototypes for lab #9

--- lab08/prj/ModulesBraginets.h
+++ lab08/prj/ModulesBraginets.h
@@ -1,7 +1,20 @@
 #ifndef MODULESBRAGINETS_H_INCLUDED
 #define MODULESBRAGINETS_H_INCLUDED
 
+#define Size 12
 double s_calculation(double x, double y, double z);
+
+float count_electrycity(float num);
+
+struct avr_temp
+{
+    float Celcius = 0;
+    float Fahrenheit = 0;
+};
+
+avr_temp count_tempreture(int mid_temp[], int n);
+
+int count_bit(long int number);
 
 
 #endif // MODULESBRAGINETS_H_INCLUDED
